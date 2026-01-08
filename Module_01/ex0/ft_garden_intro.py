1266f15f9a78a737996c405baa3d388f681829f1
Margarida Botelho Antunes <mbotelho@c1r5s10.42lisboa.com>
2026-01-08 16:22:58 +0000

    added files

---
+++ Module_01/ex0/ft_garden_intro.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def ft_garden_intro():
+    plantname = "Rose"
+    heigth = 25
+    age = 30
+
+    print("=== Welcome to My Garden ===")
+    print(f'Plant: {plantname}')
+    print(f'Heigth: {heigth}cm')
+    print(f'Age: {age} days\n')
+    print("=== End of Program ===")
+
+
+if __name__ == "__main__":
+    ft_garden_intro()
