a0549d39745fd23f936c51d87306773056b86ee6
megyant <megyant@gmail.com>
2026-01-09 12:23:53 +0000

    added file

--- Module_01/ex1/ft_garden_data.py
+++ Module_01/ex1/ft_garden_data.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
-def ft_garden_data():
-    
+class Plant:
+    def __init__(self, name, height, age):
+        self.name = name
+        self.height = height
+        self.age = age
+
+
+plants = [
+    Plant("Rose", 25, 30),
+    Plant("Sunflower", 80, 45),
+    Plant("Cactus", 15, 120)
+    ]
+
+print("=== Garden Plant Registry ===")
+
+for plant in plants:
+    print(f'{plant.name}: {plant.height}cm, {plant.age} days old')
