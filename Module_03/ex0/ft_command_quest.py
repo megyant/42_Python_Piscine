2c9680b37e4646139eba7eb97ec32c484d8ec2e0
megyant <megyant@gmail.com>
2026-02-02 12:32:17 +0000

    added files

--- Module_03/ex0/ft_command_quest.py
+++ Module_03/ex0/ft_command_quest.py
@@ -2,13 +2,15 @@ def command_interpreter():
     import sys
     number = 1
     try:
-        print(f"Program name: {sys.argv[0]}")
-        for argument in sys.argv[1:]:
-            print(f"Argument {number}: {argument}")
-            number += number
-        print(f"Total arguments: {len(sys.argv)}")
-    except ValueError:
-        print("No arguments provided!")
+        if (len(sys.argv) < 2):
+            raise ValueError("No arguments provided!")
+    except ValueError as e:
+        print(e)
+    print(f"Program name: {sys.argv[0]}")
+    for argument in sys.argv[1:]:
+        print(f"Argument {number}: {argument}")
+        number += 1
+    print(f"Total arguments: {len(sys.argv)}")
 
 
 if __name__ == "__main__":
