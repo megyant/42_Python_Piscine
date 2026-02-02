f57da04413c3a3b0309976145a3fb2400feb563b
megyant <megyant@gmail.com>
2026-02-02 12:38:44 +0000

    added files

---
+++ Module_03/ex1/ft_score_analytics.py
@@ -0,0 +1,8 @@
+# Check if there are arguments
+# Check if arguments are numbers - else raise exception
+# Print argumets after being processed in a list
+# Count the number of arguments
+# Do the average (sum(args)/len(args))
+# find high score
+# find low score
+# do max - min
