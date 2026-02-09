e0c70aa775e19d4690417498eada8ca13c8f129c
megyant <megyant@gmail.com>
2026-02-09 17:41:09 +0000

    added files

--- Module_04/ex2/ft_stream_management.py
+++ Module_04/ex2/ft_stream_management.py
@@ -1,6 +1,25 @@
+import sys
+
+
 def stream_management():
-    print("=== CYBER ARCHIVES - COMMUNICATION SYSTEM ===\n")
+    sys.stdout.write("Input Stream active. Enter archivist ID: ")
+    sys.stdout.flush()
+    id = sys.stdin.readline().strip()  # strip filters \t and \n
+
+    sys.stdout.write("Input Stream active. Enter status report: ")
+    sys.stdout.flush()
+    report = sys.stdin.readline().strip().capitalize()
+
+    sys.stdout.write(f"\n[STANDARD] Archive status from {id}: {report}\n")
+
+    sys.stderr.write("[ALERT] System diagnostic: Communication channels "
+                     "verified\n")
+
+    sys.stdout.write("[STANDARD] Data transmission complete\n")
+
+    sys.stdout.write("\nThree-channel communication test successful\n")
 
 
 if __name__ == "__main__":
+    print("=== CYBER ARCHIVES - COMMUNICATION SYSTEM ===\n")
     stream_management()
