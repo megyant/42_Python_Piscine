acbbba48ba4f8d9da4bd2ce946bd8133acfa5062
Margarida Botelho Antunes <mbotelho@c2r6s10.42lisboa.com>
2026-02-10 08:36:35 +0000

    added file

--- Module_00/ex1/ft_plot_area.py
+++ Module_00/ex1/ft_plot_area.py
@@ -1,4 +1,4 @@
 def ft_plot_area():
     length = input("Enter length: ")
     width = input("Enter width: ")
-    print("Plot area:", int(length) * int(width))
+    print(f"Plot area: {int(length) * int(width)}")
