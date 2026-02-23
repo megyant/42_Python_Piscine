22ef088f00cf96a6af5dadc2cacca4f5b561f944
Margarida Botelho Antunes <mbotelho@c1r5s10.42lisboa.com>
2026-02-23 12:58:21 +0000

    added files

---
+++ Module_07/ex0/CreatureCard.py
@@ -0,0 +1,18 @@
+from .Card import Card
+
+
+class CreatureCard(Card):
+    def __init__(self, name: str, cost: int, rarity: str, attack: int, health: int) -> None:
+        super().__init__(name, cost, rarity)
+        self.attack = attack
+        self.health = health
+        self.type = "Creature"
+
+    def get_card_info(self):
+        info = {"name": self.name, "cost": self.cost, "rarity": self.rarity,
+                "type": self.type, "attack": self.attack, "health": self.health}
+        return info
+
+    # def play(self, game_state: dict) -> dict:
+
+    # def attack_target(self, target: str) -> dict:
---
+++ Module_07/ex0/main.py
@@ -0,0 +1,14 @@
+# from ex0.Card import Card
+from .CreatureCard import CreatureCard
+
+
+def main():
+    print("=== DataDeck Card Foundation ===")
+
+    print("\nTesting Abstract Base Class Design:")
+
+    fire_dragon = CreatureCard("Fire Dragon", 5, "Legendary", 7, 5)
+
+    print("\nCreatureCard Info:")
+    info = fire_dragon.get_card_info()
+    print(info)
