261236f0840d25872086857845ac7a41951709f0
Margarida Botelho Antunes <mbotelho@c1r5s10.42lisboa.com>
2026-02-24 15:30:27 +0000

    added files

---
+++ Module_07/ex1/SpellCard.py
@@ -0,0 +1,6 @@
+class SpellCard:
+    def __init__(self, name: str, cost: int, rarity: str, effect_type: str)
+        self.name = name
+        self.cost = cost
+        self.rarity = rarity
+        self.effect_type = effect_type
