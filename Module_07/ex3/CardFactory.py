9555d508b95cf892b7a2a79278fb3bd7eb820769
mbotelho <megyant@gmail.com>
2026-03-02 18:02:10 +0000

    added files

--- Module_07/ex3/CardFactory.py
+++ Module_07/ex3/CardFactory.py
@@ -4,6 +4,7 @@ from ex0.CreatureCard import CreatureCard
 from ex1.ArtifactCard import ArtifactCard
 from ex1.SpellCard import SpellCard
 
+
 class CardFactory(ABC):
     @abstractmethod
     def create_creature(self, name_or_power: str | int | None = None) -> Card:
