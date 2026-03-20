b4aa8a9f9b4f32b673e0b64c4aa8993daa6c008e
mbotelho <megyant@gmail.com>
2026-03-20 16:08:53 +0000

    added ex1

---
+++ Module_10/ex1/higher_magic.py
@@ -0,0 +1,22 @@
+def spell_combiner(spell1: callable, spell2: callable) -> callable:
+    pass
+
+
+def power_amplifier(base_spell: callable, multiplier: int) -> callable:
+    pass
+
+
+def conditional_caster(condition: callable, spell: callable) -> callable:
+    pass
+
+
+def spell_sequence(spells: list[callable]) -> callable:
+    pass
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
